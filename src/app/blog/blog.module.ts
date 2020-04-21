76d6f07cfe620f41ff1cbda3c6890c9eac99a255
poori <pooriapoorsarvi@gmail.com>
2020-04-21 16:23:58 -0400

    blog module added

---
+++ src/app/blog/blog.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BlogComponent } from './blog/blog.component';
+
+
+
+@NgModule({
+  declarations: [BlogComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BlogModule { }
