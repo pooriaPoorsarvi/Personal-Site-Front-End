43f5dc09065aa5c53095b026b5819e191eae0c4c
poori <pooriapoorsarvi@gmail.com>
2020-04-22 00:47:39 -0400

    blog posts linking completed

--- src/app/blog/blog-list/blog-list.component.ts
+++ src/app/blog/blog-list/blog-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-blog-list',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router : Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  navigateToPost(){
+    this.router.navigate(["blog", "blog-post"]);
   }
 
 }
