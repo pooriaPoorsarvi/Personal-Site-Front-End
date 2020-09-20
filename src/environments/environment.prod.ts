d1389740c7fcf89fa36cacfd4cc6ea88ca9835da
poori <pooriapoorsarvi@gmail.com>
2020-09-20 16:32:19 -0400

    back end address updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import {backEndRoutes} from './back-end-route';
 
 export const environment = {
   production: true,
-  apiUrl: 'http://accessedportfolio-env.eba-4vsyc2gh.us-east-2.elasticbeanstalk.com',
+  apiUrl: 'http://back-end.ppt-productions.com/',
   routes: backEndRoutes
 };
 
