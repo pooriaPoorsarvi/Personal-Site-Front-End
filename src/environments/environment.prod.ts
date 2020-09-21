994202d07f48949ae0340226520539ddad61f785
poori <pooriapoorsarvi@gmail.com>
2020-09-20 23:44:22 -0400

    travis updated with nginx

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import {backEndRoutes} from './back-end-route';
 
 export const environment = {
   production: true,
-  apiUrl: 'http://back-end.ppt-productions.com/',
+  apiUrl: 'https://back-end.ppt-productions.com/',
   routes: backEndRoutes
 };
 
