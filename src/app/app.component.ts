46431bb82076bd3ea83e5ef8a40855cedbe8bd13
poori <pooriapoorsarvi@gmail.com>
2020-09-21 22:41:30 -0400

    https redirection added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Personal-Site-Front-End';
+
+  constructor(private router: Router, private location: Location){}
+
+
+  ngOnInit(){
+    if (location.protocol !== 'https:' && ! location.host.startsWith('localhost')) {
+      location.replace(`https:${location.href.substring(location.protocol.length)}`);
+    }
+  }
+
+
 }
